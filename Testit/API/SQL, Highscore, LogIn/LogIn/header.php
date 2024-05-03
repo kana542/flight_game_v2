872b305bd85f66314398039938b5c9466b317443
makyo <jonathan.shi@metropolia.fi>
2024-05-03 18:17:06 +0300

    ensimmäiset kivut

---
+++ Testit/API/SQL, Highscore, LogIn/LogIn/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Navigation Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="navbar">
+        <a href="#home">Home</a>
+        <a href="#signup">Signup</a>
+        <a href="#login">Login</a>
+    </div>

2f73e283cd07cfaa62673eb2da1913020f8ae408
makyo <jonathan.shi@metropolia.fi>
2024-05-03 23:28:27 +0300

    kipu#2

---
+++ Testit/API/SQL, Highscore, LogIn [ABANDONED]/LogIn/index.php
@@ -0,0 +1,12 @@
+<?php
+    include_once'header.php';
+?>
+    <div class="content">
+        <div class="box">Box 1</div>
+        <div class="box">Box 2</div>
+        <div class="box">Box 3</div>
+    </div>
+
+<?php
+    include_once'.php';
+?>
---
+++ Testit/API/SQL, Highscore, LogIn [ABANDONED]/LogIn/signup.php
@@ -0,0 +1,21 @@
+<?php
+    include_once'header.php';
+?>
+    <section class="signup-form">
+        <h2>Sign up</h2>
+        <div class="signup-form-form">
+            <form action="signup.inc.php" method="post">
+                <input type="text" name="name" placeholder="Fullname..." >
+                <input type="text" name="email" placeholder="Email..." >
+                <input type="text" name="uid" placeholder="Username..." >
+                <input type="password" name="pwd" placeholder="Password..." >
+                <input type="password" name="pwdrepeat" placeholder="Repeat Password..." >
+                <button type="submit" name="submit">Sign Up</button>
+            </form>
+        </div>
+    </section>
+    
+    #error message here
+<?php
+    include_once'footer.php';
+?>
